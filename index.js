3dcd146f1d61f678fcba3db78cbc04130daa64f8
Veronica Flores <veronica.marie.flores@gmail.com>
2018-08-21 14:17:02 -0600

    push

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ let server = express()
 let bp = require('body-parser')
 let port = 3000
 let cors = requires(cors())
+
+require('./server-assets/bp/index')
+
+server.use(bp.json())
+server.use(bp.urlencoded({
+    extended: true
+}))
+
+let carRoutes = require('./server-assets/routes/car-routes')
+
+
+server.use('api/pizza', carRoutes)
+
+server.listen(port, ()=>{
+    console.log('Running on Port:', port)
+})
